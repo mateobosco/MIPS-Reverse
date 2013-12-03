2a86bf64c3dabcc8c6a1460918efdfc41614342a
mateo.bosco@gmail.com <mateo.bosco@gmail.com@df7e9153-16cb-cfd4-7aee-3b2ddef37798>
2013-12-03 05:10:04 +0000

    TP TERMINADO

--- computadoras/tp/compile.sh
+++
@@ -1,3 +0,0 @@
-gcc -c reverse.S
-gcc -c ./sys_mmap/mymalloc.S -o ./mymalloc.o
-gcc mymalloc.o reverse.o main.c -o tp1
--- computadoras/tp/sys_mmap/mymalloc.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MYMALLOC_H_INCLUDED_
-#define _MYMALLOC_H_INCLUDED_
-
-extern void *mymalloc(size_t);
-extern void *myrealloc(void*,size_t);
-extern void myfree(void *);
-
-#endif
